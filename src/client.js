fd2c807d4a8f3b14a3a01d4e071bef46ca5adf77
ANS <36783010+answebdev@users.noreply.github.com>
2022-05-06 21:23:40 -0700

    add styles

--- src/client.js
+++ src/client.js
@@ -3,5 +3,6 @@ import sanityClient from '@sanity/client';
 export default sanityClient({
   projectId: 'lg4b3sfh', // find this at manage.sanity.io or in your sanity.json
   dataset: 'production', // this is from those question during 'sanity init'
+  apiVersion: '2022-05-06',
   useCdn: true,
 });
